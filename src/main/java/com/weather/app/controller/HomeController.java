ffabde9fee7bea50252258282f14da0eb3ad99e0
John Xie <xiechangchun5566@163.com>
2020-12-28 22:34:37 +0800

    Removes useless references

--- src/main/java/com/weather/app/controller/HomeController.java
+++ src/main/java/com/weather/app/controller/HomeController.java
@@ -8,14 +8,12 @@ import com.weather.app.service.ICityService;
 import com.weather.app.service.IWeatherApi;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
